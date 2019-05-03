ff0c9be1cb574127cf1b5978be4640fbf7c3f82d
Kevin Ruiz Flores <rescuepymes@gmail.com>
2019-05-03 12:38:37 -0600

    IRepository

---
+++ CinemaExpress.Web/Data/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CinemaExpress.Web.Data
+{
+    public interface IRepository
+    {
+        void AddMovie(Movie movie);
+        Movie GetMovie(int id);
+        IEnumerable<Movie> GetMovies();
+        bool MovieExists(int id);
+        void RemoveMovie(Movie movie);
+        Task<bool> SaveAllAsync();
+        void UpdateMovie(Movie movie);
+    }
+}
---
+++ CinemaExpress.Web/Data/Repository.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaExpress.Web.Data
+{
+    public class Repository : IRepository
+    {
+        private readonly DataContext context;
+
+        public Repository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return this.context.Movies.OrderBy(m => m.Título);
+        }
+
+        public Movie GetMovie(int id)
+        {
+            return this.context.Movies.Find(id);
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            this.context.Movies.Add(movie);
+        }
+
+        public void UpdateMovie(Movie movie)
+        {
+            this.context.Movies.Update(movie);
+        }
+
+        public void RemoveMovie(Movie movie)
+        {
+            this.context.Movies.Remove(movie);
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await this.context.SaveChangesAsync() > 0;
+        }
+
+        public bool MovieExists(int id)
+        {
+            return this.context.Movies.Any(p => p.Id == id);
+        }
+
+    }
+}
