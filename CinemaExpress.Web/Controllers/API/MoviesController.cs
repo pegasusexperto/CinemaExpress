ed8b540a247d60154e6e6abe927806611880548d
Kevin Ruiz Flores <rescuepymes@gmail.com>
2019-05-03 19:30:44 -0600

    API Creation

---
+++ CinemaExpress.Web/Controllers/API/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using CinemaExpress.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaExpress.Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+    public class MoviesController : Controller
+    {
+        private readonly IMovieRepository movieRepository;
+
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            this.movieRepository = movieRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetMovies()
+        {
+            return Ok(this.movieRepository.GetAll());
+
+        }
+
+    }
+}
+
